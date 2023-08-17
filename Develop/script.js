a65d81d9255d7fc11a8c4cae4c4ca7d457df9a05
Steven Gresham <sbjg2545@gmail.com>
2023-08-16 21:59:45 -0700

    moved passwordCharacters array so it will be empty before the function runs

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,12 @@ var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}','|',';',':',',','.','<','>','?'];
 var passwordLength;
-var passwordCharacters = [];
+
 
 // Write password to the #password input
 function writePassword() {
+  //Moved passwordCharacters so it will be empty before generatePassword is ran
+  var passwordCharacters = [];
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
